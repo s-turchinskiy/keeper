b82c089135f80f164623fd58d976d66d82bab283
Stanislav Turchinskiy <s.turchinskiy@yandex.ru>
2025-12-21 09:13:00 +0300

    iter1

--- internal/client/cmds/cmds_handlers.go
+++ internal/client/cmds/cmds_handlers.go
@@ -157,7 +157,7 @@ func createSecretDeleteCommand() func(cmd *cobra.Command, args []string) error {
 		uuid := args[0]
 
 		service := getServiceFromCommand(cmd)
-		err := service.DeleteSecret(context.Background(), uuid)
+		_, err := service.DeleteSecret(context.Background(), uuid)
 		if err != nil {
 			return err
 		}
